130539dd0ea6c2ea31230540556bcbbb29b92828
sslee <sangsulee@gmarket.com>
2022-04-19 11:22:37 +0900

    Feat: 축소/확대 애니메이션, 양옆 카드 살짝 미리보이기

--- app/src/main/java/com/lilcode/test/viewpagercardview/MainActivity.kt
+++ app/src/main/java/com/lilcode/test/viewpagercardview/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.lilcode.test.viewpagercardview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
@@ -20,10 +26,14 @@ class MainActivity : AppCompatActivity() {
 
         var adapter = Adapter(models, this)
         viewPager.adapter = adapter
-        viewPager.setPadding(30, 0, 30, 0)
+        viewPager.setPadding(12, 0, 12, 0)
 
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
                 super.onPageScrolled(position, positionOffset, positionOffsetPixels)
             }
 
@@ -37,5 +47,41 @@ class MainActivity : AppCompatActivity() {
         })
 
 
+        val currentVisibleItemPx = dp2px(this, 39f)
+        viewPager.addItemDecoration(object : RecyclerView.ItemDecoration() {
+            override fun getItemOffsets(
+                outRect: Rect,
+                view: View,
+                parent: RecyclerView,
+                state: RecyclerView.State
+            ) {
+                outRect.right = currentVisibleItemPx
+                outRect.left = currentVisibleItemPx
+            }
+        })
+
+        val nextVisibleItemPx = dp2px(this, 27f)
+        val pageTranslationX = nextVisibleItemPx + currentVisibleItemPx
+        viewPager.offscreenPageLimit = 1
+
+        val transform = CompositePageTransformer()
+        transform.addTransformer(MarginPageTransformer(8))
+
+        transform.addTransformer(ViewPager2.PageTransformer { view: View, position: Float ->
+            view.translationX = -pageTranslationX * (position) // 양옆 카드 살짝 미리 보이기
+
+            // 축소/확대 애니메이션
+            val v = 1 - Math.abs(position)
+            view.scaleY = 0.874f + v * 0.126f
+        })
+
+        viewPager.setPageTransformer(transform)
+
+
     }
+}
+
+fun dp2px(ctx: Context, dp: Float): Int {
+    val scale: Float = ctx.getResources().getDisplayMetrics().density
+    return (dp * scale + 0.5f).toInt()
 }
